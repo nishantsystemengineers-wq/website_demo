221a14d0c75873c5b69f7aa3397dc9d7163addff
Nishant Kumar <nishant.systemengineers@gmail.com>
2025-09-08 13:54:34 +0530

    Enable static export in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // enables static export instead of next export
 };
 
 export default nextConfig;
